e2e0cd3f76b5ef1bcf2c38fa206a70a2f768b8ce
Adnan Najjar <adnan@kalvad.com>
2026-09-07 10:30:57 +0400

    feat: incus instead of ssh

--- inventory.py
+++ inventory.py
@@ -2,27 +2,15 @@ wg_listen_port = 51820
 
 hosts = [
     (
-        "10.16.251.9",
-        {
-            "wg_ip": "192.168.2.1/24",
-            "ssh_user": "root",
-            "ssh_key": "~/.ssh/lab_key",
-        },
+        "@incus/vm1",
+        {"wg_ip": "192.168.2.1/24"},
     ),
     (
-        "10.16.251.201",
-        {
-            "wg_ip": "192.168.2.2/24",
-            "ssh_user": "root",
-            "ssh_key": "~/.ssh/lab_key",
-        },
+        "@incus/vm2",
+        {"wg_ip": "192.168.2.2/24"},
     ),
     (
-        "10.16.251.29",
-        {
-            "wg_ip": "192.168.2.3/24",
-            "ssh_user": "root",
-            "ssh_key": "~/.ssh/lab_key",
-        },
+        "@incus/vm3",
+        {"wg_ip": "192.168.2.3/24"},
     ),
 ]
